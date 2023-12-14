8c0331ab89100269398f6b761776a263fa10145b
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2023-12-14 12:50:01 +0100

    Añadido el español latino como idioma

--- myapp/public/js/embedBuilder/start/script.js
+++ myapp/public/js/embedBuilder/start/script.js
@@ -5,9 +5,17 @@ window.onload = async () => {
     await myDB.initDB("database")
     await refreshData()
     user = new User()
+
+    document.getElementById("langLatamSpanish").addEventListener("mouseenter", randomFlag)
   } catch (e) {
     console.error(e)
   } finally {
     loader.disable()
   }
+}
+
+function randomFlag() {
+  flag = ["ar","cl","co","cr","do","ec","gt","hn","ht","mx","ni","pa","pe","py","uy","ve"]
+  rand = Math.floor(Math.random()*flag.length)
+  document.getElementById("flagLatamSpanish").src = `./images/flags/es_${flag[rand]}.png`
 }
